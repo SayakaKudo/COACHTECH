d0f5adeeb3c40441b797814989ff4e9a1c114081
Sayaka Kudo <snoopyisnotcat@gmail.com>
2021-06-22 00:03:35 +0900

    20210622

---
+++ pp-vue/src/components/CommonFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="footer">
+    <h1>estra inc.</h1>
+    <p>Copyright 2019 estra inc. All Rights Reserved.</p>
+  </footer>
+</template>
+
+<style>
+.footer {
+  height: 80px;
+  text-align: center;
+  line-height: 40px;
+  color: #fff;
+  background-color: black;
+}
+
+</style>
