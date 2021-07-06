91be3167d590530929c48a14dadc681e32eecb03
Sayaka Kudo <snoopyisnotcat@gmail.com>
2021-07-06 23:55:59 +0900

    20210707

---
+++ router-pj/src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h2>About</h2>
+    <router-link :to="{name:'Home'}">遷移</router-link>
+  </div>
+</template>
---
+++ router-pj/src/views/Home.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="home">
+    <h2>Home</h2>
+    <router-link :to="{name:'About'}">遷移</router-link>
+  </div>
+</template>
